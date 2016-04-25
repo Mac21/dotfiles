659a30d06aa59f4e5668bc0f39c45f9ada43f4fe
Mac21 <russ.tolle@gmail.com>
2016-04-25 14:25:46 -0600

    Removed everything in bundle directory and updated / organized the update_bundles script.

--- update_bundles.sh
+++ update_bundles.sh
@@ -1,33 +1,55 @@
 #!/usr/bin/env ruby
 
 git_bundles = [
-  "git://github.com/astashov/vim-ruby-debugger.git",
+  # Start Vim - Completion / FS plugins.
   "git://github.com/ervandew/supertab.git",
   "git://github.com/godlygeek/tabular.git",
-  "git://github.com/hallison/vim-rdoc.git",
-  "git://github.com/pangloss/vim-javascript.git",
+  "git://github.com/scrooloose/syntastic.git",
   "git://github.com/scrooloose/nerdtree.git",
-  "git://github.com/tpope/vim-cucumber.git",
+  "git://github.com/tomtom/vim-tcomment.git",
+  #"git://github.com/xolox/vim-easytags",
+  #"git://github.com/alvan/vim-closetag",
+  # End Vim - Completion plugins.
+
+  # Start Vim - Ruby plugins.
+  #"git://github.com/hallison/vim-rdoc.git",
+  #"git://github.com/tpope/vim-cucumber.git",
+  #"git://github.com/astashov/vim-ruby-debugger.git",
+  #"git://github.com/tpope/vim-rails.git",
+  #"git://github.com/vim-ruby/vim-ruby.git",
+  #"git://github.com/tsaleh/vim-shoulda.git",
+  # End Vim - Ruby plugins.
+
+  # Start Vim - Syntax Highlighting plugins.
+  "git://github.com/pangloss/vim-javascript.git",
+  "git://github.com/tsaleh/vim-matchit.git",
+  "git://github.com/tpope/vim-markdown.git",
+  "git://github.com/tmux-plugins/vim-tmux.git",
+  # End Vim - Syntax Highlighting plugins.
+
+  # Start Vim - git plugins.
   "git://github.com/tpope/vim-fugitive.git",
   "git://github.com/tpope/vim-git.git",
-  "git://github.com/tpope/vim-haml.git",
-  "git://github.com/tpope/vim-markdown.git",
-  "git://github.com/tpope/vim-rails.git",
+  "git://github.com/vim-scripts/Gist.vim.git",
+  # End Vim - git plugins.
+
+  # Start Vim - SASS/HAML/SCSS plugins.
+  #"git://github.com/tpope/vim-haml.git",
+  # End Vim - SASS/HAML/SCSS plugins.
+
+  # Start Vim - Vim builtin wrappers plugins.
   "git://github.com/tpope/vim-repeat.git",
   "git://github.com/tpope/vim-surround.git",
-  "git://github.com/tpope/vim-vividchalk.git",
-  "git://github.com/tsaleh/taskpaper.vim.git",
-  "git://github.com/tsaleh/vim-matchit.git",
-  "git://github.com/tsaleh/vim-shoulda.git",
-  "git://github.com/tsaleh/vim-tcomment.git",
-  "git://github.com/tsaleh/vim-tmux.git",
-  "git://github.com/vim-ruby/vim-ruby.git",
-  "git://github.com/vim-scripts/Gist.vim.git",
-  "git://github.com/scrooloose/syntastic.git",
+  # End Vim - Vim builtin wrappers plugins.
+
+  # Start Vim - Vim interface plugins.
   "git://github.com/vim-airline/vim-airline",
   "git://github.com/xolox/vim-misc",
-  "git://github.com/xolox/vim-easytags",
-  #"git://github.com/alvan/vim-closetag",
+  # End Vim - Vim interface plugins.
+
+  # Start Vim - Mac Only plugins.
+  #"git://github.com/davidoc/taskpaper.vim.git",
+  # End Vim - Mac Only plugins.
 ]
 
 vim_org_scripts = [
