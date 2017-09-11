8eabc4c70ce40f11b4fc0bbc43103883c903bc92
Mac21 <russ.tolle@gmail.com>
2017-09-10 23:18:29 -0600

    Added code to see if bundles directory exists.

--- update_bundles.sh
+++ update_bundles.sh
@@ -86,24 +86,29 @@ require 'fileutils'
 require 'open-uri'
 
 bundles_dir = File.join(File.dirname(__FILE__), "bundle")
-FileUtils.cd(bundles_dir)
+if Dir.exist?(bundles_dir)
+  FileUtils.cd(bundles_dir)
+else
+  Dir.mkdir(bundles_dir, 0755)
+  FileUtils.cd(bundles_dir)
+end
 
 puts "trashing everything (lookout!)"
-Dir["*"].each {|d| FileUtils.rm_rf d }
-
-git_bundles.each do |url|
-  dir = url.split('/').last.sub(/\.git$/, '')
-  puts "\n###### Installing #{url} into #{dir} ######"
-  `git clone #{url} #{dir}`
-  FileUtils.rm_rf(File.join(dir, ".git"))
-  puts "###### Installed #{dir} ######\n"
-end
-
-vim_org_scripts.each do |name, script_id, script_type|
-  puts "downloading #{name}"
-  local_file = File.join(name, script_type, "#{name}.vim")
-  FileUtils.mkdir_p(File.dirname(local_file))
-  File.open(local_file, "w") do |file|
-    file << open("http://www.vim.org/scripts/download_script.php?src_id=#{script_id}").read
-  end
-end
+#Dir["*"].each {|d| FileUtils.rm_rf d }
+#
+#git_bundles.each do |url|
+#  dir = url.split('/').last.sub(/\.git$/, '')
+#  puts "\n###### Installing #{url} into #{dir} ######"
+#  `git clone #{url} #{dir}`
+#  FileUtils.rm_rf(File.join(dir, ".git"))
+#  puts "###### Installed #{dir} ######\n"
+#end
+#
+#vim_org_scripts.each do |name, script_id, script_type|
+#  puts "downloading #{name}"
+#  local_file = File.join(name, script_type, "#{name}.vim")
+#  FileUtils.mkdir_p(File.dirname(local_file))
+#  File.open(local_file, "w") do |file|
+#    file << open("http://www.vim.org/scripts/download_script.php?src_id=#{script_id}").read
+#  end
+#end
