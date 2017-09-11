3a6bf8e85c7383bc4e55ce576483f397739b7f61
Mac21 <russ.tolle@gmail.com>
2017-09-10 23:20:09 -0600

    Forgot to uncomment code after testing.

--- update_bundles.sh
+++ update_bundles.sh
@@ -94,21 +94,21 @@ else
 end
 
 puts "trashing everything (lookout!)"
-#Dir["*"].each {|d| FileUtils.rm_rf d }
-#
-#git_bundles.each do |url|
-#  dir = url.split('/').last.sub(/\.git$/, '')
-#  puts "\n###### Installing #{url} into #{dir} ######"
-#  `git clone #{url} #{dir}`
-#  FileUtils.rm_rf(File.join(dir, ".git"))
-#  puts "###### Installed #{dir} ######\n"
-#end
-#
-#vim_org_scripts.each do |name, script_id, script_type|
-#  puts "downloading #{name}"
-#  local_file = File.join(name, script_type, "#{name}.vim")
-#  FileUtils.mkdir_p(File.dirname(local_file))
-#  File.open(local_file, "w") do |file|
-#    file << open("http://www.vim.org/scripts/download_script.php?src_id=#{script_id}").read
-#  end
-#end
+Dir["*"].each {|d| FileUtils.rm_rf d }
+
+git_bundles.each do |url|
+  dir = url.split('/').last.sub(/\.git$/, '')
+  puts "\n###### Installing #{url} into #{dir} ######"
+  `git clone #{url} #{dir}`
+  FileUtils.rm_rf(File.join(dir, ".git"))
+  puts "###### Installed #{dir} ######\n"
+end
+
+vim_org_scripts.each do |name, script_id, script_type|
+  puts "downloading #{name}"
+  local_file = File.join(name, script_type, "#{name}.vim")
+  FileUtils.mkdir_p(File.dirname(local_file))
+  File.open(local_file, "w") do |file|
+    file << open("http://www.vim.org/scripts/download_script.php?src_id=#{script_id}").read
+  end
+end
