64a445c95913204ed552848679c98fffffb9ad63
Mac21 <russ.tolle@gmail.com>
2017-10-16 00:08:21 -0600

    Add setup script for arch linux.

---
+++ arch/setup.sh
@@ -0,0 +1,107 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Installing network packages"
+sudo pacman -S ifplugd
+
+echo "Installing Display Server, WM, and DM"
+sudo pacman -S xorg xf86-video-intel
+
+echo "Installing prerequisites for pacaur..."
+sudo pacman -S base-devel fakeroot jshon expac
+
+echo "Installing cower"
+mkdir /tmp/tmp-cower
+cd /tmp/tmp-cower
+curl -L https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=cower -o PKGBUILD
+gpg --recv-keys 1EB2638FF56C0C53
+makepkg -si
+cd -
+
+echo "Installing pacaur..."
+mkdir /tmp/tmp-pacaur
+cd /tmp/tmp-pacaur
+curl -L https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=pacaur -o PKGBUILD
+makepkg
+sudo pacman --noconfirm -U *.tar.xz
+cd -
+
+echo "Installing base packages..."
+pacaur --noconfirm --noedit -S \
+  termite-git \
+  bash-completion
+
+echo "Installing Python dependencies..."
+pacaur --noconfirm --noedit -S \
+  python-basiciw \
+  python-netifaces \
+  python-yaml \
+  python-pillow \
+  python-urllib3 \
+  python2-suds
+
+echo "Installing window manager dependencies..."
+pacaur --noconfirm --noedit -S \
+  xcb-util-keysyms \
+  xcb-util-wm \
+  xcb-util-cursor \
+  yajl \
+  startup-notification \
+  libev
+
+echo "Installing tools..."
+pacaur --noconfirm --noedit -S \
+  i3-gaps \
+  i3lock \
+  i3blocks-gaps-git \
+  i3status-git \
+  gsimplecal \
+  feh \
+  acpi \
+  xdotool \
+  pulseaudio-ctl \
+  pavucontrol \
+  network-manager-applet \
+  networkmanager-openvpn \
+  imagemagick \
+  dunst \
+  python \
+  python-pip \
+  python2-pip \
+  compton-git \
+  ttf-font-awesome \
+  ohsnap \
+  ttf-hack \
+  powerline-fonts-git \
+  thunar \
+  thunar-archive-plugin \
+  file-roller \
+  tumbler \
+  eog \
+  tk \
+  aspell-en \
+  evince \
+  rofi \
+  libmtp \
+  gvfs-mtp \
+  xtitle-git \
+  openssh \
+  arandr \
+  xclip \
+  xedgewarp-git \
+  unclutter-xfixes-git \
+  youtube-dl \
+  slop \
+  maim \
+  neofetch-git \
+  w3m \
+  htop \
+  bluez \
+  bluez-utils \
+  blueman
+
+
+echo "Installing some perl stuff..."
+pacaur --noconfirm --noedit -S \
+  perl-anyevent-i3 \
+  perl-json-xs \
