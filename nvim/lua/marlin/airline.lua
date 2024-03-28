7ee0fc5e9387f765374d54508de9260043cacd41
Mac21 <russ.tolle@gmail.com>
2024-03-28 09:01:04 -0600

    WIP: add config for airline. Still need to figure out how to enable fugitive through lua

---
+++ nvim/lua/marlin/airline.lua
@@ -0,0 +1,8 @@
+vim.g.airline_powerline_fonts = 1
+vim.g.airline_theme = 'violet'
+vim.o.noshowmode = true
+vim.o.laststatus = 2
+
+print(vim.g.airline)
+-- vim.g.airline:extensions:fugitiveline:enabled = 1
+-- vim.g.airline#parts#ffenc#skip_expected_string = 'utf-8[unix]'
