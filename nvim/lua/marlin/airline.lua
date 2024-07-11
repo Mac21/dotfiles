b7c2582823de9e68737c8eaf766af8943f3e523a
Mac21 <russ.tolle@gmail.com>
2024-07-11 08:50:20 -0600

    fix changed option of noshowmode to showmode

--- nvim/lua/marlin/airline.lua
+++ nvim/lua/marlin/airline.lua
@@ -1,6 +1,6 @@
 vim.g.airline_powerline_fonts = 1
 vim.g.airline_theme = 'violet'
-vim.o.noshowmode = true
+vim.o.showmode = false
 vim.o.laststatus = 2
 
 -- vim.g.airline:extensions:fugitiveline:enabled = 1
