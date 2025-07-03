b37a0e45b1e708b3f57d9f1a0d31ac827c24aafb
Mac21 <russ.tolle@gmail.com>
2025-07-03 12:08:11 -0600

    Disable null-ls

--- nvim/lua/marlin/null-ls.lua
+++ nvim/lua/marlin/null-ls.lua
@@ -1,9 +1,9 @@
-local null_ls = require("null-ls")
-
-null_ls.setup({
-    sources = {
-        null_ls.builtins.code_actions.impl,
-        null_ls.builtins.formatting.gofmt,
-        null_ls.builtins.formatting.goimports
-    }
-})
+--local null_ls = require("null-ls")
+--
+--null_ls.setup({
+--    sources = {
+--        null_ls.builtins.code_actions.impl,
+--        null_ls.builtins.formatting.gofmt,
+--        null_ls.builtins.formatting.goimports
+--    }
+--})
